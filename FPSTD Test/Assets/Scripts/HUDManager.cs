0740d049adc6f63c854d61ea756df25b9cf1a036
Leo Perugini <xweetyk@gmail.com>
2018-09-02 20:27:57 -0300

    UI Update
    -Hace falta incluir input manager para android
    -No puedo testear en android ;-;

---
+++ FPSTD Test/Assets/Scripts/HUDManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUDManager : MonoBehaviour {
+
+	[SerializeField] Text WallCount;
+	[SerializeField] WallsManager _WManager;
+	private int _Wcount;
+	void Update () {
+		_Wcount = _WManager.freeWallCount;
+		WallCount.text = "Turrets: " + (_Wcount.ToString ());
+	}
+}
