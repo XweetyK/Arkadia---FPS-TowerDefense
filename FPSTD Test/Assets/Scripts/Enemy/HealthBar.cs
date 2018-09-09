a4c1c775aba8142a7151b8d137f0480adee53190
Leo Perugini <xweetyk@gmail.com>
2018-09-09 19:06:18 -0300

    EnemyHealthBar
    -Barra de salud ahora visible
    
    IVXCA43FOJUW6PZAORSW4Z3PEBYXKZJANBQWGZLSEBSWYIDUOJQWEYLKN4QHI33EN4QHS3Z7

---
+++ FPSTD Test/Assets/Scripts/Enemy/HealthBar.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour {
+
+	[SerializeField] private Image _bar;
+	private Life _lifeManager;
+	private int _startLife;
+	private int _updatedLife;
+
+	void Start(){
+		_lifeManager = gameObject.GetComponent<Life> ();
+		_startLife = _lifeManager.LifeGetter;
+		_updatedLife = _lifeManager.LifeGetter;
+	}
+	void Update(){
+		_updatedLife = _lifeManager.LifeGetter;
+		_bar.fillAmount = (float)_updatedLife / _startLife;
+	}
+}
---
+++ FPSTD Test/Assets/Scripts/Managers/UIRotation.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIRotation : MonoBehaviour {
+
+	private GameObject _mainCamera;
+	void Start(){
+		_mainCamera = GameObject.FindGameObjectWithTag("MainCamera");
+	}
+
+	void Update(){
+		transform.rotation = _mainCamera.transform.rotation;
+	}
+}
