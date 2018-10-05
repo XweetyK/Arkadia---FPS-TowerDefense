47857a7ad0f8a99e03abbd5abb73706e2774a1b4
Gaspar Nuñez <gaspacho09@hotmail.com>
2018-10-05 20:32:26 -0300

    MissingShots Fix
    You miss 100% of the bullets you dont Instantiate

--- FPSTD Test/Assets/Scripts/Managers/TurretShooter.cs
+++ FPSTD Test/Assets/Scripts/Managers/TurretShooter.cs
@@ -4,28 +4,23 @@ using UnityEngine;
 
 public class TurretShooter : MonoBehaviour {
 	[SerializeField] private GameObject _bulletPrefab;
-	[SerializeField] private float _delay;
+	[SerializeField] private float _fireRate = 1.0f;
 	[SerializeField] private LayerMask _layer;
 	[Tooltip("Try to use the same as 'Range' in TurretMov")]
 	[SerializeField] private float _shootRange;
 	private GameObject _bullet;
-	private bool _active;
 	private WallClass _wall;
+	private float nextFire = 0.0f;
 
 	void Start(){
 		_wall = gameObject.GetComponentInParent<WallClass>();
-		_active = false;
 	}
 	void Update(){
 		if (_wall.Placed == true) {
 			RaycastHit hit;
-			if (Physics.Raycast (transform.position, transform.forward, out hit, _shootRange, _layer)) {
-				if (!_active) {
+			if (Physics.Raycast (transform.position, transform.forward, out hit, _shootRange, _layer) && Time.time > nextFire){
 					Shoot ();
-					_active = true;
-				}
-			} else {
-				_active = false;
+					nextFire = Time.time + _fireRate;
 			}
 		}
 	}
@@ -33,9 +28,6 @@ public class TurretShooter : MonoBehaviour {
 		_bullet = Instantiate (_bulletPrefab);
 		_bullet.transform.position = this.transform.position;
 		_bullet.transform.rotation = this.transform.rotation;
-		if (_wall.Active) {
-			Invoke ("Shoot", _delay);
-		}
 	}
 	void OnDrawGizmos(){
 		Gizmos.DrawRay(transform.position, transform.forward*_shootRange);
