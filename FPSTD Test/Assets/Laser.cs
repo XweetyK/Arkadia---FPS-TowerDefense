a9019e520233ca5857c9be6e7c29a3a3414e7a50
Gaspar Nuñez <gaspacho09@hotmail.com>
2018-11-06 14:31:30 -0300

    WIP granade launcher and Laser sight

---
+++ FPSTD Test/Assets/Laser.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour {
+	private 
+	void Start () {
+		
+	}
+
+	void Update () {
+		
+	}
+}
